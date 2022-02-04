831c9fa189442f7aee07ecd62acfb8358f229637
Celal Anak <anakcelal@hotmail.com>
2022-02-04 09:21:17 +0300

    PatikaDev C# First Repostrory

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace console_programlama
+{
+
+    public class prgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("İsminizi Girin");
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("Soyisim Girin");
+            string sName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + fName + " " + sName);
+        }
+
+    }
+}
+
